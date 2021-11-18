c400687e311049dd4b3f8088e51435d2ae3c51b2
@KING_TECHPRO <94400110+KINGTECHPRO@users.noreply.github.com>
2021-11-18 18:37:18 +0530

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,76 @@
+/* Copyright (C) 2021 KING_TECHPRO.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+KING_TECHPRO - Nirmal Gamage
+*/
+
+
+const chalk = require('chalk');
+const {WAConnection} = require('@adiwajshing/baileys');
+const {StringSession} = require('./kingtech_pro/');
+const fs = require('fs');
+
+async function king_techpro () {
+    const conn = new WAConnection();
+    const Session = new StringSession();  
+    conn.version = [2, 2126, 14]
+    conn.logger.level = 'warn';
+    conn.regenerateQRIntervalMs = 50000;
+    
+    conn.on('🔄 connecting', async () => {
+        console.log(`${chalk.green.bold('Queen')}${chalk.blue.bold(' TECH')}
+${chalk.white.italic('KING_TECHPRO Sting session')}
+
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
+    });
+    
+
+	conn.on('open', async () => {
+		console.log(
+			chalk.green.bold('KING_TECHPRO QR Code: '),
+			'TECH;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				)
+		);
+		await conn.sendMessage(
+			conn.user.jid,
+			'TECH;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				),
+			MessageType.text
+		);
+		if (conn.user.jid.startsWith('94')) {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ HI I'AM NIRMAL GAMAGE Meka denna epa katawath ' + conn.user.name + '* ⚠️',
+				MessageType.text
+			);
+		} else {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ HI I'AM NIRMAL GAMAGE Please Do Not Share This Code With Anyone ' +
+					conn.user.name +
+					'* ⚠️',
+				MessageType.text
+			);
+		}
+		console.log(
+			chalk.green.bold(
+				"Meka copy karanna bari nm whatsapp eke athi bn code eka awith!\n"
+			),
+			chalk.green.bold(
+				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+			)
+		);
+		process.exit(0);
+	});
+
+	await conn.connect();
+}
+
+king_techpro()
+
